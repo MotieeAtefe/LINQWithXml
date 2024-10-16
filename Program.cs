883ff85163e34e628092ec0a4d1c9c996402b280
MotieeAtefe <motiee1377@gmaile.com>
2024-10-16 18:31:18 +0330

    transfer linq to xml

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LINQWithXml
 {
@@ -10,6 +11,17 @@ namespace LINQWithXml
     {
         static void Main(string[] args)
         {
+            string inputFilePath = "input.xml";
+            string outputFilePath = "output.xml";
+            XElement inputXml = XElement.Load(inputFilePath);
+
+            var modifiedData = from element in inputXml.Elements("Person")
+                               where (int)element.Element("Age") > 25
+                               select new XElement("ModifiedPerson", new XElement("Name", element.Element("Name").Value), new XElement("Age", (int)element.Element("Age") + 5)
+                               );
+            XElement outputXml = new XElement("modifiedData", modifiedData);  
+            outputXml.Save(outputFilePath);
+            Console.WriteLine("Data tranfer LINQ to XML", outputFilePath);
 
         }
     }
